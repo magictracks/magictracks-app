bb2dc431803f595788fe12b2c5c9c0d3cfa79e71
joeyklee <jsphknglee@gmail.com>
2018-11-28 14:17:01 -0500

    added nav highlight

--- views/browse.js
+++ views/browse.js
@@ -139,17 +139,26 @@ function MainContent(id, state, emit){
     emit("browse:filter");
   }
 
+  function highlightSelected(_db){
+
+    if(state.params.db == _db){
+      return " bg-purple white "
+    } else{
+      return " bg-washed-blue black "
+    }
+  }
+
   function FilterMenu(){
     return html`
       <menu class="w-100 pa0 ma0">
         <fieldset>
         <legend class="f7">Browse by </legend>
         <ul class="w-100 flex flex-row-ns flex-column list pl0 f6 justify-between">
-          <li class="mr2 pointer" onclick=${changeFilter} data-db="playlists">Playlists</li>
-          <li class="mr2 pointer" onclick=${changeFilter} data-db="sections">Sections</li>
-          <li class="mr2 pointer" onclick=${changeFilter} data-db="resources">Resources</li>
-          <li class="mr2 pointer" onclick=${changeFilter} data-db="users">Contributors</li>
-          <li class="mr2 pointer" onclick=${changeFilter} data-db="tags">Tags</li>
+          <li class="mr2 pointer pa2 ${highlightSelected('playlists')}" onclick=${changeFilter} data-db="playlists">Playlists</li>
+          <li class="mr2 pointer pa2 ${highlightSelected('sections')}" onclick=${changeFilter} data-db="sections">Sections</li>
+          <li class="mr2 pointer pa2 ${highlightSelected('resources')}" onclick=${changeFilter} data-db="resources">Resources</li>
+          <li class="mr2 pointer pa2 ${highlightSelected('users')}" onclick=${changeFilter} data-db="users">Contributors</li>
+          <li class="mr2 pointer pa2 ${highlightSelected('tags')}" onclick=${changeFilter} data-db="tags">Tags</li>
           <li class="mr2">Surprise Me 🎊</li>
         </ul>
         </fieldset>
